facd30d4041a0194e266809ade1e6d943e219a29
Allen Qiu <118089569+allenQiuCool2020@users.noreply.github.com>
2023-05-22 19:08:51 +0800

    2nd revise
    2nd revise

--- conversion.py
+++ conversion.py
@@ -1,14 +1,19 @@
 import pandas as pd
-# read_file = pd.read_excel("billing_320942.xlsx")
+# read the file
 
 dataframe1 = pd.read_excel("test522a.xlsx")
 
 
-# dataframe1.to_excel("test522ab.xlsx")
+# updating the description of the first column, converting to integer and adding leading zero
 dataframe1.rename(columns=({'Bill.Doc.':'VIA_B2B_BillNumber__c','BillT':'VIA_B2B_Type__c','Billing Date':'EPD_Invoice_Date__c','PayT':'VIA_B2B_PaymentTermCode__c','DstC':'VIA_B2B_CountryCode__c', 'Net Value':'VIA_B2B_TotalBillAmount__c','Sold-to pt':'VIA_B2B_CustomerCodeSoldTo__c'}), inplace=True)
 
+dataframe1["VIA_B2B_BillNumber__c"] = dataframe1["VIA_B2B_BillNumber__c"].astype(int)
+dataframe1["VIA_B2B_CustomerCodeSoldTo__c"] = dataframe1["VIA_B2B_CustomerCodeSoldTo__c"].astype(int)
 
+dataframe1["VIA_B2B_BillNumber__c"] = dataframe1["VIA_B2B_BillNumber__c"].astype(str).apply(lambda x:x.zfill(10))
 print(dataframe1)
-# dataframe1.to_excel("test522a1.xlsx", index=False)
+
+# exporting to CSV file
 dataframe1.to_csv("test522a1.csv", index=False)
-# read_file.to_csv("billing_320942.csv", index=False)
+
+
--- exceltocsv.py
+++
@@ -1,3 +0,0 @@
-# import pandas as pd
-# read_file = pd.read_excel("test522a1.xlsx")
-# read_file.to_csv("test522a1.csv", index=False)
