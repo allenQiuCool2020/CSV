ebcc2792542bd9c909994913fa076e8c608d2dc1
Allen Qiu <118089569+allenQiuCool2020@users.noreply.github.com>
2023-05-23 20:28:00 +0800

    Final clean up
    Final clean up

--- conversion.py
+++ conversion.py
@@ -7,25 +7,19 @@ lists = []
 for (root, dirs, file) in os.walk(path):
     for f in file:
         if '.XLSX' in f:
-            print(f)
             lists.append(f)
         else:
             print(f"{f} is with the wrong file format, it must be ended with XLSX from SAP")
 
 print(lists)
 for list in lists:
-    # list1 = f'"{list}"'
-    # print(list1)
     print(list)
 
 def exceltocsv(f1, *args, **kwargs):
+    
     # Read the file and converting it to dataframe
     # dataframe1 = pd.read_excel("315103.xlsx")
-
     dataframe1 = pd.read_excel(f"C:\Dev\CSV\input\{f1}")
-    # Count rows
-    # count_rows = len(dataframe1.index)
-    # print(count_rows)
 
     # Delete last row
     dataframe1.drop(dataframe1.tail(1).index, inplace=True)
